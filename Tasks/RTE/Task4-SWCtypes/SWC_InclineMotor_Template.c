f26bb5378b570959131aab059b7ca55edd14bf89
Tasnime1 <tasneemfarag68@gmail.com>
2024-02-16 15:45:03 +0200

    RTE Task4: SWC Types code generated templates and C code development using Sprint's SAAT tool.

---
+++ Tasks/RTE/Task4-SWCtypes/SWC_InclineMotor_Template.c
@@ -0,0 +1,41 @@
+/**
+ *
+ * \file SWC_InclineMotor_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: SWC_InclineMotor
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 1/22/2024 05:42 PM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_SWC_InclineMotor.h"
+
+
+/**
+ *
+ * Runnable InclineMotor_Move
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppInclineMotor_Move
+ *
+ */
+
+void InclineMotor_Move (StepMotorStepType step)
+{
+	Std_ReturnType status;
+
+	/* Server Call Points */
+	
+	/* Server Call Points */
+	if(step == PLUS)
+	{
+		status = Rte_Call_rpIOSetIncline_IOSetForward();
+	}
+	else if(step == MINUS)
+	{
+		status = Rte_Call_rpIOSetIncline_IOSetReverse();
+	}
+	
+}
---
+++ Tasks/RTE/Task4-SWCtypes/SWC_WeightSensor_Template.c
@@ -0,0 +1,37 @@
+/**
+ *
+ * \file SWC_WeightSensor_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: SWC_WeightSensor
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 12/25/2023 01:46 PM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_SWC_WeightSensor.h"
+
+
+/**
+ *
+ * Runnable WeightSensor_GetPosition
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppWeightSensor_GetWeight
+ *
+ */
+
+void WeightSensor_GetPosition (SensorWeightType* SensorWeightType_weight)
+{
+	Std_ReturnType status;
+	IoWeightSensorReadingType IoWeightSensorReadingType_weight;
+
+	/* Server Call Points */
+	status = Rte_Call_rpIOGetWeight_IOGet(&IoWeightSensorReadingType_weight);
+	
+	/*Return weight/100*/
+	SensorWeightType_weight = IoWeightSensorReadingType_weight/100;
+	
+}
+
